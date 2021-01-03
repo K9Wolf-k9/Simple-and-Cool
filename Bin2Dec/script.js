cd5a86962fd6a4cd86399a03546ee7fd0486b7dd
Kauê Santana <kauesantana2010@hotmail.com>
2021-01-03 17:35:24 -0300

    check 0 and 1 as input, and comments
    new learnings, basic arrow functions, split() and map()

--- Bin2Dec/script.js
+++ Bin2Dec/script.js
@@ -2,29 +2,32 @@ function convert() {
     var bin = document.getElementById('txtbin').value
     var dec = 0 //1001
 
-    for(var i = 0; i < bin.length; i++)
-    {
-        var num = parseInt(bin[i])
-        if(num != 0 || num != 1)
-        {
-            window.alert('Enter a binary number')
-            break
-        }
-    }
-    
-    if (bin.length == 0)
+    if (bin.length == 0) //bin === ''
     {
         window.alert('Enter a binary number')
-    }
+    } 
     else
     {
+
+        bin.split('').map(char => { 
+            if (char !== '0' && char !== '1') return alert('Enter a binary number')
+        })
+        dec = parseInt(bin, 2)
+        
+        document.getElementById('txtres').value = dec
+    }
+}
+
         /*for (var i = bin.length-1; i >= 0; i--) //-1 porque o indice começa no 0
         {
             dec +=  parseInt(bin[i]) * Math.pow(2, bin.length-1-i)  0 += 1 * 2^4-1-3 = 1 / 1 += 0 * 2^4-1-2 = 1 / 1 += 0 * 2^4-1-1 = 1 / 1 += 1 * 2^4-1-0 = 9
         }
         */
-            dec = parseInt(bin, 2)
-        
-        document.getElementById('txtres').value = dec
-    }
-}
+
+        /*
+            bin.split('').map(caract)
+
+            function caract(char){
+                if (char !== '0' && char !== '1') return alert('Enter a binary number')
+            }
+        */ 
